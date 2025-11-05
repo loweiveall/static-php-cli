199138b5d295bd6541936716443b830f2112b285
daicanglong <1059979944@qq.com>
2025-11-05 15:29:29 +0800

    mosquitto lib-depends

--- src/SPC/builder/unix/library/libmosquitto.php
+++ src/SPC/builder/unix/library/libmosquitto.php
@@ -10,6 +10,10 @@ trait libmosquitto
 {
     protected function build(): void
     {
-        UnixCMakeExecutor::create($this)->addConfigureArgs('-DBUILD_STATIC_LIBS=ON')->build();
+        UnixCMakeExecutor::create($this)->addConfigureArgs(
+            '-DBUILD_STATIC_LIBS=ON',
+            '-DWITH_STATIC_LIBRARIES=ON',
+            '-DWITH_SHARED_LIBRARIES=OFF',
+        )->build();
     }
 }
