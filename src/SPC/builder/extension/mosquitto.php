0bc07f203f555b03cd8bc814ead2f888c709021d
daicanglong <1059979944@qq.com>
2025-11-25 14:47:07 +0800

    getUnixConfigureArg

--- src/SPC/builder/extension/mosquitto.php
+++ src/SPC/builder/extension/mosquitto.php
@@ -26,7 +26,7 @@ class mosquitto extends Extension
 
     public function getUnixConfigureArg(bool $shared = false): string
     {
-        return '--with-mosquitto=' . BUILD_ROOT_PATH;
+        return '--with-mosquitto=' . ($shared ? 'shared,' : '') . BUILD_ROOT_PATH;
     }
 
     public function getWindowsConfigureArg($shared = false): string
